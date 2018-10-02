fb0bdcede216bd7b6ccec1f0fac0920a8b7cd5a7
Ali Sebt <a.sebt@ramand.local>
2018-10-02 12:24:16 +0330

    added 3 pages

--- App.js
+++ App.js
@@ -1,52 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {create} from 'apisauce';
+import {Platform, StyleSheet, Text, View,ActivityIndicator,FlatList,Image,TouchableOpacity,ScrollView,Dimensions} from 'react-native';
+import HTML from 'react-native-render-html';
+import { createStackNavigator } from 'react-navigation';
 
-import {Platform, StyleSheet, Text, View,ActivityIndicator,FlatList,Image} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
- 
 
 type Props = {};
-export default class App extends Component<Props> {
+class App extends Component<Props> {
   constructor(props) {
     super(props);
     this.state ={ isLoading: true}
   }
-
+  
   componentDidMount(){
     const api = create({
-      baseURL: 'http://core.ucan.ir/mobile/request.asmx',
+      baseURL: 'https://core.ucan.ir/mobile/request.asmx',
       headers: {'Content-Type': 'application/json'}
     })
     api.post('/GetCategoryList', JSON.stringify({request: {}}))
     .then((response)=>{
-      //alert(JSON.stringify(response.data.Result.GetCategoryList.length) );
-      //alert(response.data.Result.GetCategoryList[1].Title)
-      // for (var index = 0; index < response.data.Result.GetCategoryList.length-1; index++) {
-      //   alert(response.data.Result.GetCategoryList[index].Title);
-      // }
       this.setState({
         isLoading: false,
         dataSource: response.data.Result.GetCategoryList
      });
-
     })
-    .then(console.log)
+    .then(()=>{
+      //alert("Check your internet connection.");
+      //console.log
+    })
   }
   
   render() {
@@ -58,36 +39,153 @@ export default class App extends Component<Props> {
       )
     }
      return(
-      
       <FlatList
           data={this.state.dataSource}
+          keyExtractor={ (item) => item.CategoryID }
           renderItem={(
             {item}) => 
-          <View style={styles.row}>
-            
-            <Image style={styles.rowimage} source={{uri:item.Image}}  ></Image>
-            <Text style={styles.rowtext}>{item.Title} </Text>
-          </View>
+            <TouchableOpacity onPress={()=>{
+                  //alert(item.CategoryID);
+                    /* 1. Navigate to the Details route with params */
+                  this.props.navigation.navigate('Details', {
+                    itemId: item.CategoryID,
+                    otherParam: 'anything you want here',
+                  });
+              }}>
+              <View style={styles.row}>
+                <Image style={styles.rowimage} source={{uri:item.Image}}  ></Image>
+                <Text style={styles.rowtext}>{item.Title} </Text>
+              </View>
+            </TouchableOpacity>
           }
         />
      )
   }
 }
 
+class DetailsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const api = create({
+      baseURL: 'https://core.ucan.ir/mobile/request.asmx',
+      headers: {'Content-Type': 'application/json'}
+    })
+    api.post('/GetContentByCategoryList', JSON.stringify({request: {RequestID:itemId}}))
+    .then((response)=>{
+      this.setState({
+        isLoading: false,
+        dataSource: response.data.Result.GetContentByCategoryList
+     });
+    })
+    .then(()=>{
+      //alert("Check your internet connection.");
+      //console.log
+    })
+  }
+
+  render() {
+   
+    //const otherParam = navigation.getParam('otherParam', 'some default value');
+    return (
+      <FlatList
+          data={this.state.dataSource}
+          keyExtractor={ (item) => item.CategoryID }
+          renderItem={(
+            {item}) => 
+            <TouchableOpacity onPress={()=>{
+                  //alert(item.CategoryID);
+                    /* 1. Navigate to the Details route with params */
+                  this.props.navigation.navigate('ContentDetails', {
+                    itemId: item.ContentID,
+                    otherParam: 'anything you want here',
+                  });
+              }}>
+              <View style={styles.row}>
+                <Image style={styles.rowimage} source={{uri:item.ThumbImage}}  ></Image>
+                <Text style={styles.rowtext}>{item.Title} </Text>
+              </View>
+            </TouchableOpacity>
+          }
+        />
+    );
+  }
+}
+
+class ContentDetailsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const api = create({
+      baseURL: 'https://core.ucan.ir/mobile/request.asmx',
+      headers: {'Content-Type': 'application/json'}
+    })
+    api.post('/GetContent', JSON.stringify({request: {RequestID:itemId}}))
+    .then((response)=>{
+      this.setState({
+        isLoading: false,
+        dataSource: response.data.Result
+     });
+    })
+    .then(()=>{
+      //alert("Check your internet connection.");
+      //console.log
+    })
+  }
+
+  render() {
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return (
+      <ScrollView style={{flex:1}} >
+          <Image style={styles.landscapeImage} source={{uri:this.state.dataSource.ThumbImage}}  ></Image>
+          <HTML html={this.state.dataSource.Body} imagesMaxWidth={Dimensions.get('window').width} />
+          
+      </ScrollView>
+    );
+  }
+}
+
+export default createStackNavigator({
+  Home: {
+    screen: App
+  },
+  Details:{
+    screen: DetailsScreen
+  },
+  ContentDetails:{
+    screen: ContentDetailsScreen
+  }
+});
+
 const styles = StyleSheet.create({
   row:{
-    flex:1,
-    
-    backgroundColor:'#ECECEd',
-    marginBottom:5,
-    flexDirection:'row'
+    flex:1,backgroundColor:'#ECECEd',marginBottom:5,flexDirection:'row'
   },
   rowtext:{
     fontSize:20,
   },
   rowimage:{
-    width:50,
-    height:50 ,
-    margin:5
+    width:50,height:50 ,margin:5
+  },
+  landscapeImage:{
+    height:150,resizeMode:'stretch'
   },
 });
